eb531075c29de4109fa648dd6c8ddaefdd7ce855
Pravin Bhagwat <85389806+pravinbhagwat@users.noreply.github.com>
2023-09-08 12:13:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-//your code here
+document.addEventListener('DOMContentLoaded', function () {
+    const inputField = document.getElementById('input');
+    const buttons = document.querySelectorAll('button');
+    let currentInput = '';
+
+    buttons.forEach(button => {
+        button.addEventListener('click', function () {
+            const buttonText = button.textContent;
+
+            if (button.id === 'ans') {
+                try {
+                    currentInput = eval(currentInput);
+                    if (isNaN(currentInput)) {
+                        currentInput = 'NaN';
+                    }
+                } catch (error) {
+                    currentInput = 'Error';
+                }
+            } else if (button.id === 'clr') {
+                currentInput = '';
+            } else {
+                currentInput += buttonText;
+            }
+
+            inputField.value = currentInput;
+        });
+    });
+});
